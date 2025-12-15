7a9bbc30fc86db96742b17dd46a65453a7a32211
harsha Donakanti <user.email>
2025-12-15 12:41:47 +0530

    added db

--- dataBase/db.js
+++ dataBase/db.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
-const connectToDb = async ()=>{
-  try{
-    await mongoose.connect(process.env.MONGODB_URL)
-  }catch(err){
-    console.log(err);
-    
+const connectToDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
   }
-}
-export default connectToDb
+};
+export default connectToDb;
